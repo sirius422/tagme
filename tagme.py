3500a2a8da752ccca63cf276b6f9e5031c426618
sirius42 <infivoid@gmail.com>
2020-03-23 12:26:58 +0800

    Add a workaround to single quote

--- tagme.py
+++ tagme.py
@@ -4,17 +4,21 @@ import os
 path = os.getcwd()
 for root, dirs, files in os.walk(path):
     for filename in files:
-        if filename != '.DS_Store':  # warning: may not suitable for PNG files, I'm still work on it
+        if filename != '.DS_Store' or filename.endswith(
+                'original'):
             fullname = os.path.splitext(filename)[0]  # get the filename< without extensions>
+
+            # add escape characters to avoid error in shells
+            filename = filename.replace("'", r"\'")  # no single quote in path
+            filename = filename.replace(' ', r'\ ')
+            filename = filename.replace('(', r'\(')
+            filename = filename.replace(')', r'\)')
+
             fulltag = (fullname.split("@")[-1]).split(",")
             fullpath = os.path.join(root, filename)
 
-            # add escape characters to avoid error in shells
-            fullpath = fullpath.replace(' ', r'\ ')
-            fullpath = fullpath.replace('(', r'\(')
-            fullpath = fullpath.replace(')', r'\)')
-
             for tag in fulltag:
-                cmd = f"exiftool -keywords+='{tag}' -charset iptc=UTF8 -iptc:codedcharacterset=utf8 {fullpath}"
-                # use '' on linux\unix
+                cmd = """exiftool -keywords+="{}" -charset iptc=UTF8 -iptc:codedcharacterset=utf8 {}""".format(tag,
+                                                                                                                fullpath)
+                # seems like that double quote also works on linux, use this because of the keyword like Girls'frontline
                 os.system(cmd)
